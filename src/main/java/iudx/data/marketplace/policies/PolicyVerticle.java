70c5b0493d5fc85089ffcda7f9a886d2a3c5f004
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-12-01 14:31:53 +0530

    Update PolicyVerticle

--- src/main/java/iudx/data/marketplace/policies/PolicyVerticle.java
+++ src/main/java/iudx/data/marketplace/policies/PolicyVerticle.java
@@ -13,6 +13,7 @@ import iudx.data.marketplace.postgres.PostgresServiceImpl;
 import java.util.Map;
 
 import static iudx.data.marketplace.common.Constants.POLICY_SERVICE_ADDRESS;
+import static iudx.data.marketplace.common.Constants.POSTGRES_SERVICE_ADDRESS;
 import static iudx.data.marketplace.policies.util.Constants.DB_RECONNECT_ATTEMPTS;
 import static iudx.data.marketplace.policies.util.Constants.DB_RECONNECT_INTERVAL_MS;
 
@@ -25,43 +26,13 @@ public class PolicyVerticle extends AbstractVerticle {
     private VerifyPolicy verifyPolicy;
     private GetPolicy getPolicy;
     private CatalogueService catalogueService;
-    private PgPool pool;
     private Vertx vertx;
 
     @Override
     public void start() throws Exception {
         vertx = Vertx.vertx();
-        /* Database properties */
-        /* Database Properties */
-        String databaseIp = config().getString("databaseIP");
-        int databasePort = config().getInteger("databasePort");
-        String databaseSchema = config().getString("databaseSchema");
-        String databaseName = config().getString("databaseName");
-        String databaseUserName = config().getString("databaseUserName");
-        String databasePassword = config().getString("databasePassword");
-        int poolSize = config().getInteger("poolSize");
-        Map<String, String> schemaProp = Map.of("search_path", databaseSchema);
-
-        /* Set Connection Object and schema */
-        PgConnectOptions connectOptions =
-                new PgConnectOptions()
-                        .setPort(databasePort)
-                        .setHost(databaseIp)
-                        .setProperties(schemaProp)
-                        .setDatabase(databaseName)
-                        .setUser(databaseUserName)
-                        .setPassword(databasePassword)
-                        .setReconnectAttempts(DB_RECONNECT_ATTEMPTS)
-                        .setReconnectInterval(DB_RECONNECT_INTERVAL_MS);
-
-        /* Pool options */
-        PoolOptions poolOptions = new PoolOptions().setMaxSize(poolSize);
-
-        /* Create the client pool */
-        this.pool = PgPool.pool(vertx, connectOptions, poolOptions);
-
         catalogueService = new CatalogueService(vertx, config());
-        postgresService = new PostgresServiceImpl(pool);
+        postgresService = PostgresService.createProxy(vertx, POSTGRES_SERVICE_ADDRESS);
         deletePolicy = new DeletePolicy(postgresService);
         getPolicy = new GetPolicy(postgresService);
         createPolicy = new CreatePolicy(postgresService, catalogueService);
