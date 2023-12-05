bb3b5a00bf0b5edfe5024dc44bbd1d1806011a81
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-12-05 10:10:10 +0530

    Update flyway schema file

--- src/main/resources/db/migration/V1_1__init-tables.sql
+++ src/main/resources/db/migration/V1_1__init-tables.sql
@@ -1,5 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE policy_status AS ENUM
+(
+'ACTIVE',
+'DELETED'
+);
+
+CREATE TYPE payment_status_type as ENUM
+(
+    'SUCCEEDED',
+    'FAILED'
+);
+
 CREATE TYPE status_type AS ENUM
 (
     'ACTIVE',
@@ -7,83 +19,115 @@ CREATE TYPE status_type AS ENUM
     'EXPIRED'
 );
 
-CREATE TYPE payment_status as ENUM
+CREATE TABLE IF NOT EXISTS user_table
 (
-    'SUCCEEDED',
-    'FAILED'
+ _id uuid NOT NULL,
+  email_id varchar NOT NULL,
+	first_name varchar NOT NULL,
+	last_name varchar NOT NULL,
+	created_at timestamp without time zone NOT NULL,
+	updated_at timestamp without time zone NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY (_id)
+
 );
+
+
+CREATE TABLE IF NOT EXISTS resource_entity
+(
+    _id uuid NOT NULL,
+    resource_name varchar UNIQUE NOT NULL,
+	provider_id uuid NOT NULL,
+	resource_server_url varchar NOT NULL,
+	accessPolicy varchar NOT NULL,
+	created_at timestamp without time zone NOT NULL,
+	updated_at timestamp without time zone NOT NULL,
+	CONSTRAINT resource_pk PRIMARY KEY (_id),
+	CONSTRAINT provider_id_fk FOREIGN KEY (provider_id) REFERENCES user_table(_id)
+);
+
 
 CREATE TABLE IF NOT EXISTS product
 (
-    productID varchar UNIQUE NOT NULL,
-    providerID varchar NOT NULL,
-    providerName varchar NOT NULL,
+    product_id uuid UNIQUE NOT NULL,
+    resource_id uuid NOT NULL,
     status status_type NOT NULL,
     created_at timestamp without time zone NOT NULL,
     modified_at timestamp without time zone NOT NULL,
-    CONSTRAINT product_pk PRIMARY KEY (productID)
+    CONSTRAINT product_pk PRIMARY KEY (product_id)
 );
 
-CREATE TABLE IF NOT EXISTS dataset
+
+
+CREATE TABLE IF NOT EXISTS policy
 (
-    datasetID varchar UNIQUE NOT NULL,
-    datasetName varchar UNIQUE NOT NULL,
-    accessPolicy varchar NOT NULL,
-    providerID varchar NOT NULL,
-    providerName varchar NOT NULL,
-    totalResources integer NOT NULL,
+    _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    resource_id uuid NOT NULL,
+    constraints json NOT NULL,
+    provider_id uuid NOT NULL,
+    consumer_email_id varchar NOT NULL,
+    expiry_at timestamp without time zone,
+    expiry_amount varchar,
+    status policy_status NOT NULL,
+    product_id uuid NOT NULL,
     created_at timestamp without time zone NOT NULL,
     modified_at timestamp without time zone NOT NULL,
-    CONSTRAINT dataset_pk PRIMARY KEY (datasetID)
+    CONSTRAINT resource_id_fk FOREIGN KEY(resource_id) REFERENCES resource_entity(_id),
+    CONSTRAINT provider_id_fk FOREIGN KEY (provider_id) REFERENCES user_table (_id),
+    CONSTRAINT product_id_fk FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
-CREATE TABLE IF NOT EXISTS product_dataset_relation
+
+
+CREATE TABLE IF NOT EXISTS product_resource_relation
 (
-    productID varchar REFERENCES product (productID),
-    datasetID varchar REFERENCES dataset (datasetID),
+    product_id uuid REFERENCES product (product_id),
+    resource_id uuid REFERENCES resource_entity (_id),
     _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    CONSTRAINT pkey PRIMARY KEY (_id)
+    CONSTRAINT _id_pkey PRIMARY KEY (_id)
 );
+
 
 CREATE TABLE IF NOT EXISTS product_variant
 (
-    _id uuid NOT NULL,
-    productVariantName varchar NOT NULL,
-    productID varchar NOT NULL,
-    providerID varchar NOT NULL,
-    datasets varchar[] NOT NULL,
-    datasetIDs varchar[] NOT NULL,
-    datasetCapabilities varchar[][] NOT NULL,
-    price numeric NOT NULL,
+    _id uuid NOT NULL PRIMARY KEY,
+    product_variant_name varchar NOT NULL,
+    product_id uuid NOT NULL,
+    provider_id uuid NOT NULL,
+    resource_name varchar[] NOT NULL,
+    resource_ids uuid[] NOT NULL,
+    resource_capabilities varchar[][] NOT NULL,
+    price varchar NOT NULL,
     validity numeric NOT NULL,
     status status_type NOT NULL,
     created_at timestamp without time zone NOT NULL,
     modified_at timestamp without time zone NOT NULL,
-    CONSTRAINT pv_pk PRIMARY KEY
-    (
-      _id
-    ),
-    CONSTRAINT product_fk
-        FOREIGN KEY(productID)
-            REFERENCES product(productID)
+    CONSTRAINT product_id_fk FOREIGN KEY (product_id)
+    REFERENCES product (product_id),
+    CONSTRAINT provider_id_fk FOREIGN KEY (provider_id)
+    REFERENCES user_table (_id)
 );
+
+
 
 CREATE TABLE IF NOT EXISTS purchase
 (
-    consumerID varchar NOT NULL,
-    productID varchar NOT NULL,
-    paymentStatus payment_status NOT NULL,
-    paymentTime timestamp without time zone NOT NULL,
+    _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
+    consumer_id uuid NOT NULL,
+    product_id uuid NOT NULL,
+    payment_status payment_status_type NOT NULL,
+    payment_time timestamp without time zone NOT NULL,
     expiry numeric NOT NULL,
     product_variant json NOT NULL,
     created_at timestamp without time zone NOT NULL,
     modified_at timestamp without time zone NOT NULL,
-    _id uuid DEFAULT uuid_generate_v4 () NOT NULL,
-    CONSTRAINT purchase_pk PRIMARY KEY (_id),
-    CONSTRAINT product_fk
-        FOREIGN KEY(productID)
-            REFERENCES product(productID)
+    CONSTRAINT purchase_id_pk PRIMARY KEY (_id),
+    CONSTRAINT consumer_id_fk FOREIGN KEY (consumer_id)
+    REFERENCES user_table (_id),
+    CONSTRAINT product_id_fk FOREIGN KEY (product_id)
+    REFERENCES product (product_id)
 );
+
+
 
 -- modified_at column function
 CREATE
@@ -103,30 +147,38 @@ RETURN NEW;
 END;
 $$ language 'plpgsql';
 
+
 ---
 -- Triggers
 ---
 
--- product table
-CREATE TRIGGER update_product_created BEFORE INSERT ON product FOR EACH ROW EXECUTE PROCEDURE update_created ();
-CREATE TRIGGER update_product_modified BEFORE INSERT
-OR UPDATE ON
-   product FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+-- user_table
+CREATE trigger update_user_table_created before insert on user_table for each row EXECUTE procedure update_created();
+CREATE trigger update_user_table_modified before insert or update on user_table for each row EXECUTE procedure update_modified ();
 
--- dataset table
-CREATE TRIGGER update_dataset_created BEFORE INSERT ON dataset FOR EACH ROW EXECUTE PROCEDURE update_created ();
-CREATE TRIGGER update_dataset_modified BEFORE INSERT
-OR UPDATE ON
-   dataset FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+-- resource_entity
+CREATE trigger update_resource_entity_created before insert on resource_entity for each row EXECUTE procedure update_created();
+CREATE trigger update_resource_entity_modified before insert or update on resource_entity for each row EXECUTE procedure update_modified();
 
--- product variant table
+-- product
+CREATE TRIGGER update_product_created BEFORE INSERT ON product FOR EACH ROW EXECUTE PROCEDURE update_created();
+CREATE TRIGGER update_product_modified BEFORE INSERT OR UPDATE ON product FOR EACH ROW EXECUTE PROCEDURE update_modified();
+
+-- policy
+CREATE TRIGGER update_policy_created BEFORE INSERT ON policy FOR EACH ROW EXECUTE PROCEDURE update_created();
+CREATE TRIGGER update_policy_modified BEFORE INSERT OR UPDATE ON policy FOR EACH ROW EXECUTE PROCEDURE update_modified();
+
+-- product_variant
 CREATE TRIGGER update_pv_created BEFORE INSERT ON product_variant FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_pv_modified BEFORE INSERT
 OR UPDATE ON
    product_variant FOR EACH ROW EXECUTE PROCEDURE update_modified ();
 
--- purchase table
+-- purchase
 CREATE TRIGGER update_purchase_created BEFORE INSERT ON purchase FOR EACH ROW EXECUTE PROCEDURE update_created ();
 CREATE TRIGGER update_purchase_modified BEFORE INSERT
 OR UPDATE ON
    purchase FOR EACH ROW EXECUTE PROCEDURE update_modified ();
+
+
+
