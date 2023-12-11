3be5c43daadf72ace6642ffedcc38c72ceb18a29
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-12-11 14:48:35 +0530

    Update failsafe

--- src/main/java/iudx/data/marketplace/policies/CreatePolicy.java
+++ src/main/java/iudx/data/marketplace/policies/CreatePolicy.java
@@ -1,11 +1,16 @@
 package iudx.data.marketplace.policies;
 
+import com.github.rholder.retry.*;
+import com.google.common.base.Predicates;
+import dev.failsafe.Failsafe;
+import dev.failsafe.RetryPolicy;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.sqlclient.Tuple;
+import iudx.data.marketplace.apiserver.exceptions.DxRuntimeException;
 import iudx.data.marketplace.common.CatalogueService;
 import iudx.data.marketplace.common.ResponseUrn;
 import iudx.data.marketplace.postgres.PostgresService;
@@ -13,9 +18,15 @@ import iudx.data.marketplace.postgres.PostgresServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static iudx.data.marketplace.apiserver.util.Constants.TITLE;
@@ -108,49 +119,108 @@ public class CreatePolicy {
     String insertPolicy = "INSERT INTO POLICY (_id, resource_id, constraints, provider_id, consumer_email_id, expiry_amount, status, product_id)" +
             " VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
 
+    /* purchase related info */
+        UUID purchaseId = UUID.randomUUID();
+        String paymentStatus = "SUCCEEDED";
+        LocalDateTime paymentTime = LocalDateTime.now();
+        int expiry = 4;
+        JsonObject productVariant = new JsonObject().put("someKey", "someValue");
+        JsonObject purchaseJson = new JsonObject()
+                .put("id", purchaseId.toString())
+                .put("consumer_id", consumerId.toString())
+                .put("product_id", productId.toString())
+                .put("payment_status", paymentStatus)
+                .put("payment_time", paymentTime.toString())
+                .put("expiry", expiry)
+                .put("product_variant", productVariant);
+
+        String insertPurchase = "INSERT INTO PURCHASE (_id, consumer_id, product_id, payment_status, " +
+                "payment_time, expiry, " +
+                "product_variant) VALUES ($1, $2, $3, $4,$5, $6, $7);";
+
+
+
+
     var consumerInsertion =
         insertQueries(userInsertion, consumerInsertionJson, "consumer insertion ");
 
 
-    var providerInsertion =
-            consumerInsertion.compose(consumerInsertedSuccessfully -> {
-                return insertQueries(userInsertion, providerInsertionJson, "provider insertion ");
-            });
+//    var providerInsertion =
+//            consumerInsertion.compose(consumerInsertedSuccessfully -> {
+//                return insertQueries(userInsertion, providerInsertionJson, "provider insertion ");
+//            });
+//
+//    var resourceInsertion =
+//        providerInsertion.compose(
+//            providerInsertedSuccessfully -> {
+//              return insertQueries(
+//                  resourceEntityInsertion, resourceEntityJson, "resource insertion");
+//            });
+//
+//    var productInsertion =
+//        resourceInsertion.compose(
+//            resourceInsertedSuccessfully -> {
+//              return insertQueries(insertProduct, productJson, "product insertion ");
+//            });
+//
+//    var policyInsertion =
+//        productInsertion.compose(
+//            productInsertedSuccessfully -> {
+//              return insertQueries(insertPolicy, policyJson, "policy insertion ");
+//            });
+//
+//        var purchaseInsertion =
+//                policyInsertion.compose(
+//                        policyInsertedSuccessfully -> {
+//                            return insertQueries(insertPurchase, purchaseJson, "purchase insertion");
+//                        }
+//                );
 
-    var resourceInsertion =
-        providerInsertion.compose(
-            providerInsertedSuccessfully -> {
-              return insertQueries(
-                  resourceEntityInsertion, resourceEntityJson, "resource insertion");
-            });
+//        policyInsertion.onComplete(
+//        handler -> {
+//          if (handler.succeeded()) {
+//            promise.complete(
+//                new JsonObject()
+//                    .put(TYPE, ResponseUrn.SUCCESS_URN.getUrn())
+//                    .put(TITLE, "Insertion successful"));
+//          } else {
+//              handler.cause().printStackTrace();
+//            promise.fail(
+//                new JsonObject()
+//                    .put(TYPE, ResponseUrn.DB_ERROR_URN.getUrn())
+//                    .put(TITLE, "Insertion Failed")
+//                    .encode());
+//          }
+//        });
 
-    var productInsertion =
-        resourceInsertion.compose(
-            resourceInsertedSuccessfully -> {
-              return insertQueries(insertProduct, productJson, "product insertion ");
-            });
+        RetryPolicy<Object> retryPolicy = RetryPolicy.builder()
+                .handle(Exception.class)
+                .onRetry(event -> {
+                    LOGGER.info("retrying ----->");
+                })
+                .withDelay(Duration.ofSeconds(10))
+                .withBackoff(1, 3, ChronoUnit.SECONDS)
+                .withMaxRetries(3)
+                .build();
 
-    var policyInsertion =
-        productInsertion.compose(
-            productInsertedSuccessfully -> {
-              return insertQueries(insertPolicy, policyJson, "policy insertion ");
+    Failsafe.with(retryPolicy)
+        .getAsyncExecution(
+            handler -> {
+              /* inserting same consumer information again which will lead to postgres throwing -- error */
+              insertQueries(userInsertion, consumerInsertionJson, "consumer insertion ")
+                  .onComplete(
+                      insertQueryHandler -> {
+                        if (insertQueryHandler.succeeded()) {
+                          handler.recordResult(insertQueryHandler.result());
+                        } else {
+                          handler.recordException(
+                              new DxRuntimeException(
+                                  500,
+                                  ResponseUrn.DB_ERROR_URN,
+                                  insertQueryHandler.cause().getMessage()));
+                        }
+                      });
             });
-
-    policyInsertion.onComplete(
-        handler -> {
-          if (handler.succeeded()) {
-            promise.complete(
-                new JsonObject()
-                    .put(TYPE, ResponseUrn.SUCCESS_URN.getUrn())
-                    .put(TITLE, "Insertion successful"));
-          } else {
-            promise.fail(
-                new JsonObject()
-                    .put(TYPE, ResponseUrn.DB_ERROR_URN.getUrn())
-                    .put(TITLE, "Insertion Failed")
-                    .encode());
-          }
-        });
 
     return promise.future();
   }
