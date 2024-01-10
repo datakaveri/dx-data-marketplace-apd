14cb347f417ff47adcb7955dd144b65077d9cf90
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-01-10 14:09:51 +0530

    Add auditing

---
+++ src/main/java/iudx/data/marketplace/auditing/databroker/DataBrokerService.java
@@ -0,0 +1,10 @@
+package iudx.data.marketplace.auditing.databroker;
+
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+
+public interface DataBrokerService {
+
+    Future<Void> publishMessage(String toExchange, String routingKey, JsonObject body);
+}
