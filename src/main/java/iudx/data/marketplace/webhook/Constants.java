ccdeccb7a73c4d5d8197fc4dea5ccbe5282ba07b
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-05-06 12:41:19 +0530

    Update files
    Added log to fetch the invoice id and payment status when the payment information is updated through webhook request. Removed DELETE /policies API from API docs

--- src/main/java/iudx/data/marketplace/webhook/Constants.java
+++ src/main/java/iudx/data/marketplace/webhook/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
   public static final String ORDER_ID = "order_id";
 
   public static final String UPDATE_PAYMENT_STATUS_QUERY =
-      "update $0 set payment_status = $1 where order_id = $2";
+      "update $0 set payment_status = $1 where order_id = $2 returning _id, payment_status;";
 }
