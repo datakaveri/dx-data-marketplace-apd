33914b1a9f07995c7cea6a8fcd4b55b5d46c31e3
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-06-20 17:07:09 +0530

    Cleanup files

--- src/main/java/iudx/data/marketplace/consentAgreementGenerator/util/Assets.java
+++ src/main/java/iudx/data/marketplace/consentAgreementGenerator/util/Assets.java
@@ -3,31 +3,18 @@ package iudx.data.marketplace.consentAgreementGenerator.util;
 import java.nio.file.Path;
 
 public class Assets {
-//    public static final String absolutePath1 = "src/main/java/iudx/data/marketplace/pdfGenerator";
-    public static final String ASSETS =  "assets";
     public static final String FILE_EXTENSION = ".html";
     public static final String HTML_FILE_NAME = "consentAgreement";
     private String absolutePath;
     private Path path;
- //
 
     public Assets(Path path)
     {
         this.path = path;
-//        this.path = Paths.get(HTML_FILE_NAME+FILE_EXTENSION);
         Path absolutePath = path.toAbsolutePath();
         this.absolutePath = absolutePath.toString().replace(HTML_FILE_NAME + FILE_EXTENSION, "src/main/java/iudx/data/marketplace/consentAgreementGenerator/assets");
 
     }
-//    public void setAbsolutePath(String path)
-//    {
-//        Path path = Paths.get(HTML_FILE_NAME+FILE_EXTENSION);
-//        Path absolutePath = path.toAbsolutePath();
-//        String absPath = absolutePath.toString().replace(HTML_FILE_NAME + FILE_EXTENSION, "src/main/java/iudx/data/marketplace/pdfGenerator");
-//
-//        File file = new File(path);
-//        this.absolutePath = file.getAbsolutePath();
-//    }
     public String getAbsolutePath() {
     System.out.println(this.absolutePath);
         return this.absolutePath;
