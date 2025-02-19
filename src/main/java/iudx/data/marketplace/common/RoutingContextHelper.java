999716e6b03b3196393f688dfe650464acd9f301
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2025-02-19 11:30:01 +0530

    Refactor: RoutingContextHelper

--- src/main/java/iudx/data/marketplace/common/RoutingContextHelper.java
+++ src/main/java/iudx/data/marketplace/common/RoutingContextHelper.java
@@ -53,8 +53,7 @@ public class RoutingContextHelper {
     /* allowing both the tokens to be authenticated for now */
     /* TODO: later, 401 error is thrown if the token does not contain Bearer keyword */
     String token = routingContext.request().headers().get(HEADER_BEARER_AUTHORIZATION);
-    boolean isBearerAuthHeaderPresent =
-        routingContext.request().headers().contains(HEADER_BEARER_AUTHORIZATION);
+    boolean isBearerAuthHeaderPresent = token != null && token.contains(HEADER_TOKEN_BEARER);
     if (isBearerAuthHeaderPresent && token.trim().split(" ").length == 2) {
       String[] tokenWithoutBearer = token.split(HEADER_TOKEN_BEARER);
       token = tokenWithoutBearer[1].replaceAll("\\s", "");
